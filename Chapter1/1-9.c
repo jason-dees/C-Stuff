d03b9ef3c6d28cc11a8833ce57c273f439a679af
Jason Dees <jhdees@gmail.com>
2016-11-14 18:30:32 -0800

    Adding exercise 1-9

---
+++ Chapter1/1-9.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void main(){
+    int c, wasSpace, isSpace;
+
+    wasSpace = isSpace = 0;
+
+    while ((c = getchar()) != EOF){
+        isSpace = (c == '\t' || c == '\n' || c == ' ');  
+
+        if(!wasSpace && isSpace){
+            putchar(' ');
+        }
+        else if(!isSpace){
+            putchar(c);
+        }
+        
+
+        wasSpace = isSpace;
+
+    }
+}
