c6059a18f94c748b155272f95d194ba7d3ecc24c
Jason Dees <jhdees@gmail.com>
2016-11-15 21:10:19 -0500

    Added another exercise with a histogram and it's a lot of code

---
+++ Chapter1/1-13.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+#define MAXWORDLENGTH 20
+
+void main(){
+    int c, i, wordLength, mostWords, longestWordLoop;
+
+    int lengthCounts[MAXWORDLENGTH];
+
+    wordLength = mostWords  = 0;
+    
+    for(i = 0; i < MAXWORDLENGTH; ++i){
+        lengthCounts[i] = 0;
+    } 
+
+    while((c = getchar()) != EOF){
+       if(c == ' ' || c == '\n' || c == '\t'){ //Word has ended
+           if(wordLength > 0){// doesn't count two of the above characters in a row
+               ++lengthCounts[wordLength];
+
+               if(lengthCounts[wordLength] > mostWords){
+                   mostWords = lengthCounts[wordLength]; // keeps track of the longest word
+               }
+           }
+           wordLength = 0;
+       }
+       else{
+           ++wordLength;
+       }
+    }
+    
+    for(longestWordLoop = mostWords; longestWordLoop > 0; --longestWordLoop){// Loop for the height of the histogram
+        printf("%d", longestWordLoop);
+
+        for(i = 0; i < MAXWORDLENGTH; ++i){
+            if(lengthCounts[i]){//only shows lenths that have words
+                if(lengthCounts[i] >= longestWordLoop){
+                    printf(" #");
+                }
+                else{
+                    printf("  ");
+                }
+            }
+        } 
+        putchar('\n');
+
+    }
+    printf(" ");
+    for(i = 0; i < MAXWORDLENGTH; ++i){
+        if(lengthCounts[i]){//only shows lenths that have words
+            printf("%2d", i);
+        }
+    } 
+    putchar('\n');
+}
