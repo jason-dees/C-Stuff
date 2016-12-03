06b4314b5cb43c325dc75e2c552da8197c9fe778
Jason Dees <jhdees@gmail.com>
2016-12-03 15:47:50 -0500

    More exercices

---
+++ Chapter1/1-22.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#define MAXLINE 1000
+#define FOLDAT 20
+
+int getLine(char line[], int maxline);
+void fold(char in[], int foldAt, int maxline);
+int isSpace(char c);
+int isEndLine(char c);
+
+void main(){
+    int len;
+    char line[MAXLINE];
+
+    while((len = getLine(line, MAXLINE)) > 0){
+        fold(line, FOLDAT, MAXLINE);
+    }
+}
+
+int getLine(char s[], int lim){
+    int c, i;
+    for(i = 0; i< lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i){
+        s[i] = c;
+    }
+    if(c == '\n'){
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
+
+
+void fold(char in[], int foldAt, int lim){
+    int c, i,  spaceChar, noFoldCount;
+    noFoldCount = 0;
+    spaceChar = ' ';
+    for(i = 0; i< lim - 1 && !isEndLine(c = in[i]); ++i){
+        if(noFoldCount >= foldAt && isSpace(c)){
+            noFoldCount = 0;
+            printf("\n");
+        }
+        else if(isSpace(c) && noFoldCount == 0){
+        }
+        else{
+            putchar(c);
+            noFoldCount++;
+        }
+    }
+    putchar('\n');
+}
+
+int isSpace(char c){
+    return c == ' ' || c == '\t';
+}
+int isEndLine(char c){
+    return c == EOF || c == '\n';
+}
---
+++ Chapter1/1-23.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <string.h>
+#define MAXLINE 1000
+#define SLASH_STAR 1
+#define STAR_SLASH 2 
+
+
+int getLine(char line[], int maxline);
+void removeComment(char in[]);
+int searchComment(char in[]);
+int stripComment(char in[], int start, int end);
+
+void main(){
+    int len, inComment, hasComment;
+    char line[MAXLINE];
+    inComment = hasComment = 0;
+
+    while((len = getLine(line, MAXLINE)) > 0){
+        hasComment = searchComment(line);
+        if(hasComment == SLASH_STAR){
+            inComment = SLASH_STAR;
+            printf("%s\n", line);
+        }
+        else if(hasComment == STAR_SLASH){
+            inComment = 0;
+            printf("%s\n", line);
+        }
+        else if(!inComment){
+            printf("%s\n", line);
+        }
+    }
+}
+
+int getLine(char s[], int lim){
+    int c, i;
+    for(i = 0; i< lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i){
+        s[i] = c;
+    }
+    if(c == '\n'){
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
+
+int searchComment(char in[]){
+    char* slashStar = strstr(in, "/*");
+    int slashStarPosition = -1; 
+
+    char* slashSlash = strstr(in, "//");
+    int slashSlashPosition = -1; 
+
+    char* starSlash = strstr(in, "*/");
+    int starSlashPosition = -1; 
+
+    if(slashStar != NULL){
+        slashStarPosition = slashStar - in;
+    }
+    if(slashSlash != NULL){
+        slashSlashPosition = slashSlash - in;
+    }
+    if(starSlash != NULL){
+        starSlashPosition = starSlash - in;
+    }
+    if(slashStarPosition > -1 && starSlashPosition > -1){
+        stripComment(in, slashStarPosition, starSlashPosition + 2);
+        return 0;
+    }
+    if(slashStarPosition > -1){
+        stripComment(in, slashStarPosition, strlen(in));
+        return SLASH_STAR;
+    }
+    if(slashSlashPosition > -1){
+        stripComment(in, slashSlashPosition, strlen(in));
+        return 0;
+    }
+    if(starSlashPosition > -1){
+        stripComment(in, 0, starSlashPosition + 2);
+        return STAR_SLASH;
+    }
+}
+
+int stripComment(char in[], int start, int end){
+    int i = 0;
+    for(i = start; i< end; i++){
+        in[i] = ' ';
+    }
+}
+
