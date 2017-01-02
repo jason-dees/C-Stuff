760723da3d3a61826b26b91afffd5e50c0c94d21
Jason Dees <jhdees@gmail.com>
2017-01-02 10:14:52 -0500

    Started chapter 4

---
+++ Chapter4/4-1.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#define MAX 100
+
+int getLine(char s[], int lim);
+int strrindex(char s[], char t);
+
+int main(){
+
+	int lim;
+	lim = MAX;
+	char s[lim], t[lim];
+
+	getLine(s, lim);
+
+	printf("%d\n", strrindex(s, 'a'));
+}
+
+int getLine(char s[], int lim){
+	int isValid = 1;
+	int i = 0;
+	char c;
+	while(isValid){
+		isValid = i < MAX - 1 && (c = getchar()) != '\n' && c != EOF;
+		s[i] = c;
+		++i;
+	}
+	s[i] = '\0';
+	return i;
+}
+
+int strrindex(char s[], char t){
+	int i, rightmost;
+	rightmost = -1;
+	for(i = 0; s[i] != '\0'; i++){
+		if(s[i] == t){
+			rightmost = i;
+		}
+	}
+	return rightmost;
+}
