9720110dbdde53c32d2e8f2c925738bbf71452a7
Jason Dees <jhdees@gmail.com>
2017-01-19 22:10:36 -0500

    Did another part of the exercise

--- Chapter5/5-5.strncat.c
+++ Chapter5/5-5.strncat.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 
-void strncpy(char *, char *, size_t);
+void strncat(char *, char *, size_t);
 
 int main(){
     char amessage[] = "";
     char bmessage[] = "fghi";
 
-    strncpy(amessage, bmessage, 2);
+    strncat(amessage, bmessage, 2);
     printf("%s\n", amessage);
 
-    strncpy(amessage, bmessage, 4);
+    strncat(amessage, bmessage, 4);
     printf("%s\n", amessage);
 }
 
-void strncpy(char *dest, char *src, size_t n){
+void strncat(char *dest, char *src, size_t n){
     int count = 0;
-
+    if(*dest != '\0'){
+        while(*++dest != '\0'){
+            ;
+        }
+    }
     while((*dest++ = *src++) != '\0' && ++count < n){
     }
     *dest = '\0';
