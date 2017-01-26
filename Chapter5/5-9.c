b8893309e911cc0eb40b2c945e7c3b4811a17fa3
Jason Dees <jhdees@gmail.com>
2017-01-25 20:22:29 -0500

    Fixing a bug and changing to a pointer

--- Chapter5/5-9.c
+++ Chapter5/5-9.c
@@ -4,14 +4,14 @@ int day_of_year(int, int, int);
 void month_day(int, int, int *, int *);
 
 int main(){
-	printf("%d\n", day_of_year(1978,5,55));
+	printf("%d\n", day_of_year(1978,5,20));
 	
 	int pmonth, pday;
-	month_day(1, 500, &pmonth, &pday);
+	month_day(1, 1, &pmonth, &pday);
 	printf("%d/%d\n", pmonth, pday);
 }
 
-static char daytab[2][13] = {
+static char (*days)[13] = (char [][13]){
 	{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
 	{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
 };
@@ -24,12 +24,12 @@ int day_of_year(int year, int month, int day){
 
 	leap = (year%4 == 0 && year%100) != 0 || year%400 == 0;
 
-	if(day > daytab[leap][month]){
+	if(day > days[leap][month]){
 		return -1;
 	}
 
 	for(i = 1; i< month; i++){
-		day += daytab[leap][i];
+		day += days[leap][i];
 	}
 
 	return day;
@@ -46,8 +46,8 @@ void month_day(int year, int yearday, int *pmonth, int *pday){
 		return;
 	}
 
-	for(i = 1; yearday > daytab[leap][i]; i++){
-		yearday -= daytab[leap][i];
+	for(i = 1; yearday > days[leap][i]; i++){
+		yearday -= days[leap][i];
 	}
 
 	*pmonth = i;
