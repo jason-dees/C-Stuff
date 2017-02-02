10a02cd087eee3f735a4fa3e174e103cdaa9f8fd
Jason Dees <jhdees@gmail.com>
2017-02-01 19:43:10 -0500

    Added another flag

---
+++ Chapter5/5-16.c
@@ -0,0 +1,173 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAXLINES 5000
+char *lineptr[MAXLINES];
+
+int readlines(char *lineptr[], int maxlines);
+void writelines(char *lineptr[], int nlines);
+void reverselines(char *lineptr[], int nlines);
+
+void qSort(void *lineptr[], int left, int right, int (*comp)(void *, void *), int directoryorder);
+int numcmp(char *, char *);
+
+int main(int argc, char *argv[]){
+    int nlines;
+    int direction = 1;
+    int numeric = 0;
+    int ignorecase = 0;
+    int directoryorder = 0;
+    while(argc-- > 1){
+        ++argv;
+        if(strcmp(*argv, "-n") == 0){
+            numeric = 1;
+        }
+        else if(strcmp(*argv, "-r") == 0){
+            direction = 0;
+        }
+        else if(strcmp(*argv, "-f") == 0){
+            ignorecase = 1;
+        }
+        else if(strcmp(*argv, "-d") == 0){
+            directoryorder = 1;
+        }
+    }
+    if((nlines = readlines(lineptr, MAXLINES)) >= 0){
+        qSort((void **) lineptr, 0, nlines -1,
+            (int (*)(void*, void*))(numeric ? numcmp: ignorecase ? strcasecmp : strcmp), directoryorder);
+        if(direction){
+            writelines(lineptr, nlines); 
+        }
+        else{
+            reverselines(lineptr, nlines);
+        }
+        return 0;
+    }
+    return 1;
+}
+void strclean(char *s);
+
+void qSort(void *v[], int left, int right, int (*comp)(void *, void*), int directoryorder){
+    int i, last;
+    void swap(void *v[], int, int);
+
+    if(left >= right){
+        return;
+    }
+
+    swap(v, left, (left + right)/2);
+    last = left;
+
+    char ireplace[100], leftreplace[100];
+
+    for(i = left + 1; i <= right; i++){
+        strcpy(ireplace, v[i]);
+        strcpy(leftreplace, v[left]);
+        if(directoryorder){
+            strclean(ireplace);
+            strclean(leftreplace);
+        }
+        if ((*comp)(ireplace, leftreplace) < 0){
+            swap(v, ++last, i);
+        }
+    }
+
+    swap(v, left, last);
+    qSort(v, left, last - 1, comp, directoryorder);
+    qSort(v, last + 1, right, comp, directoryorder);
+}
+
+void swap(void *v[], int i, int j){
+    void *temp;
+
+    temp = v[i];
+    v[i] = v[j];
+    v[j] = temp;
+}
+
+void strclean(char *s){
+    int c, i, j;
+    i = 0;
+    j = 0;
+    while((c = s[i++]) != '\0'){
+        if(c == ' ' || (c >= 'A' && c <='Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')){
+            s[j++] = c;            
+        }
+        else{
+        }
+    }
+    s[j] = '\0';
+}
+
+#include <stdlib.h>
+int numcmp(char *s1, char *s2){
+    double v1, v2;
+
+    v1 = atof(s1);
+    v2 = atof(s2);
+    if(v1 < v2){
+        return -1;
+    }
+    else if(v1 > v2){
+        return 1;
+    }
+    return 0;
+}
+
+#define MAXLEN 1000
+int getLine(char *, int);
+char *alloc(int);
+
+int readlines(char *lineptr[], int maxlines){
+    int len, nlines;
+    char *p, line[MAXLEN];
+
+    nlines = 0;
+    while((len = getLine(line, MAXLEN)) > 0){
+        if(nlines >= maxlines || (p = alloc(len)) == NULL){
+            return -1;
+        }
+        else {
+            line[len - 1] = '\0';
+            strcpy(p, line);
+            lineptr[nlines++] = p;
+        }
+    }
+    return nlines;
+}
+
+void writelines(char *lineptr[], int nlines){
+    int i = 0;
+    for(i = 0; i< nlines; i++){
+        printf("%s\n", lineptr[i]);
+    }
+}
+void reverselines(char *lineptr[], int nlines){
+    while(--nlines > -1){
+        printf("%s\n", lineptr[nlines]);
+    }
+}
+
+int getLine(char *s, int lim){
+    int c, i;
+    while(i++ < lim && (c = getchar()) != EOF && c != '\n'){
+        *s++ = c;
+    }
+    if(i == 1 && c == '\n'){
+        --i;
+    }
+    return i;
+}
+
+#define ALLOCSIZE 10000
+
+static char allocbuf[ALLOCSIZE];
+static char *allocp = allocbuf;
+
+char *alloc(int n){
+    if(allocbuf + ALLOCSIZE - allocp >= n){
+        allocp +=n;
+        return allocp -n;
+    }
+    return 0;
+}
