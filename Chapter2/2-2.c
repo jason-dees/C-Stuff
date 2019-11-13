85ebabea66e063bd4e272ca3a49cad08c3af4195
Jason Dees <jdees@pureromance.com>
2019-11-13 12:33:19 -0500

    Changed it to math!

--- Chapter2/2-2.c
+++ Chapter2/2-2.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
+/*
+isValid = i < lim - 1 && (c = getchar()) != '\n' && c != EOF;
+
+Write a loop equivalent to the for loop above without using && or ||.
+*/
 
 int main(){
 
 	int i, lim, c, isValid;
 	lim = 100;
 	i = 0;
-	isValid = 1;
+	isValid = 3;
 	char s[lim];
 
-	while(isValid){
-		isValid = i < lim - 1 && (c = getchar()) != '\n' && c != EOF;
-		s[i] = c;
-		++i;
+	while(isValid == 3){
+		c = getchar();
+		isValid = (i < lim - 1) + (c != '\n') + (c != EOF);
+		s[i++] = c;
 	}
     
 	s[i] = '\0';
