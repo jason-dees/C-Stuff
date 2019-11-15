b10e441749304b9ac5a960d9ec2ba226ab792db5
Jason Dees <jdees@pureromance.com>
2019-11-15 09:44:19 -0500

    I think this is it?

--- Chapter2/2-6.c
+++ Chapter2/2-6.c
@@ -6,27 +6,39 @@ Write a function setbits(x,p,y,y) that returns x with the n bits that begin at p
 of y, leaving the other bits unchanged.
 */
 
-unsigned setbits(int x, int p, int n, int y);
+unsigned setbits(unsigned x, int p, int n, unsigned y);
 unsigned getbits(unsigned x, int p, int n);
 
 int main(){
-	//0 indexed, of course
-	//x = 0100
-	//y = 0001
+	//5 = 0101
+	//6 = 0110
+	//7 = 0111
+	//8 = 1000
 	//9 = 1001
-	printf("%d\n", setbits(4, 4, 1, 1));
+	//10 = 1010
+	//11 = 1011
+	//12 = 1100
+	//13 = 1101
+	//15 = 1111
+	printf("%d\n", setbits(9, 3, 2, 11));
 }
 
 unsigned getbits(unsigned x, int p, int n){
+	printf("from %d get %d bits from position %d \n", x, n, p);
 	return (x >> (p+1-n)) & ~(~0 << n);
 }
 
-unsigned setbits(int x, int p, int n, int y){
-	//god everything i did was wrong!!!
-	int offs = (1 << (p - n)) - 1; 
-	int ons  = (1 << (p)) - offs;
-
-	x = y | (int) ons;
-
-	return x;
+unsigned setbits(unsigned x, int p, int n, unsigned y)
+{
+	int yp = getbits(y, n-1, n);
+	printf("left shift yp %d over %d bits = ", yp, p-1);
+	yp = yp << (p-1);
+	printf("%d\n", yp);
+	unsigned zeroedBits = getbits(x, p, n);
+	printf("%d\n", zeroedBits);
+	zeroedBits = zeroedBits << (p - 1);
+	printf("%d\n", zeroedBits);
+	x = x & ~zeroedBits;
+	printf("%d\n", x);
+	return x | yp;
 }
