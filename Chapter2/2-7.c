eb1fd998d7435a9c510c9eef6ce066e3829bd8af
Jason Dees <jdees@pureromance.com>
2019-11-15 11:47:59 -0500

    Update 2-7.c

--- Chapter2/2-7.c
+++ Chapter2/2-7.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <math.h>
+/*
+page 49
+Write a function invert(x,p,n) that returns x with the n bits that being at position p inverted, leaving the others 
+unchanged.
+*/
 
 unsigned setbits(unsigned x, int p, int n, int y);
 unsigned getbits(unsigned x, int p, int n);
 unsigned invert(unsigned x, int p, int n);
 
 int main(){
-	//invert of 1101
-	//           ^
-	printf("%d\n", invert(8, 3, 1));
-	printf("%d\n", invert(12, 3, 1));
+	//invert of 1000
+	printf("%d\n", invert(8, 3, 2));
+	printf("%d\n", invert(12, 3, 2));
 	printf("%d\n", invert(13, 3, 1));
 	printf("%d\n", invert(15, 3, 2));
 	//getbits is 101
@@ -24,10 +28,10 @@ unsigned invert(unsigned x, int p, int n){
 	printf("onedBits  %d\n", onedBits);
 	//Length of left shift amount the ones to match where we want to 
 	//xor/invert the bits at p through p-n
-	unsigned leftShiftAmount = (p - n + 1);
+	unsigned leftShiftAmount = (p - 1);
 	printf("Left Shift Amount %d\n", leftShiftAmount);
 	//Creates the values to xor
-	unsigned XORer = (leftShiftedOne << leftShiftAmount);
+	unsigned XORer = (onedBits << leftShiftAmount);
 	printf("XORer %d\n", XORer);
 	//^ with x
 	return x ^ XORer;
