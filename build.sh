10d52ec92346f843cab2506d98188907b8df21d6
Jason Dees <jdees@pureromance.com>
2019-11-15 11:35:29 -0500

    Made a build task for running my build shell script

--- build.sh
+++ build.sh
@@ -3,6 +3,18 @@ if [ ! -d "bin" ]; then
 mkdir bin
 fi
 #cc -g -O -c ./Shared/*.c -o ./bin/Shared.o
-cc 	./Chapter$1/$1-$2.c ./Shared/*.c -o ./bin/$1-$2.out
-./bin/$1-$2.out
+chapter=$1
+lesson=$2
+if [ -z "$lesson" ]
+then
+    search=".c"
+    replace=""
+    chapter="${1//$search/$replace}"
+    IFS='-'
+    read -ra splitted <<< "$chapter"
+    chapter="${splitted[0]}"
+    lesson="${splitted[1]}"
+fi
+cc ./Chapter${chapter}/${chapter}-${lesson}.c ./Shared/*.c -o ./bin/${chapter}-${lesson}.out
+./bin/${chapter}-${lesson}.out
 #make chapter=$1 exercise=$2
