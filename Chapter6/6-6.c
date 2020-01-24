ff539b0746a16bfb66c5a5a73bb1aee9088fd170
Jason Dees <jhdees@gmail.com>
2020-01-23 22:10:20 -0500

    Create 6-6.c

---
+++ Chapter6/6-6.c
@@ -0,0 +1,102 @@
+struct nlist{
+    struct nlist *next;
+    char *name;
+    char *defn;
+};
+
+#define MAXWORD 100
+
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "../Shared/getLine.h"
+/*
+page 145
+Implement a simple version of the #define preprocessor (i.e. no arguments) suitable for use with C programs, based on 
+the routines of this section. You may also find getch and ungetch helpful.
+*/
+
+#define HASHSIZE 101
+
+static struct nlist *hashtab[HASHSIZE];
+
+/* hash: form hash value for string s */
+unsigned hash(char *s){
+    unsigned hashval;
+    for(hashval = 0; *s != '\0'; s++){
+        hashval = *s + 31 * hashval;
+    }
+    return hashval % HASHSIZE;
+}
+
+/* lookup: look for s in hashtab */
+struct nlist *lookup(char *s){
+    struct nlist *np;
+
+    for(np = hashtab[hash(s)]; np != NULL; np = np->next) {
+        if(strcmp(s, np->name) == 0)
+            return np;
+    }
+    return NULL;
+}
+
+struct nlist *lookup(char *);
+/* install: put(name, defn) in hashtab */
+struct nlist *install(char *name, char *defn){
+    struct nlist *np;
+    unsigned hashval;
+
+    if((np = lookup(name)) == NULL){ //Not Found
+        np = (struct nlist *)malloc(sizeof(*np));
+        if(np == NULL || (np->name = strdup(name)) == NULL) {
+            return NULL;
+        }
+        hashval = hash(name);
+        np->next = hashtab[hashval];
+        hashtab[hashval] = np;
+    }
+    else{
+        free((void *) np->defn); //free previous defn
+    }
+    if((np->defn = strdup(defn)) == NULL){
+        return NULL;
+    }
+    return np;
+}
+
+void undef(char *);
+void undef(char *name){
+    struct nlist *np;
+    unsigned hashval; 
+
+    if((np = lookup(name)) == NULL){ //Not Found
+        return;
+    }
+    else {
+       hashval = hash(name); 
+       np = hashtab[hashval];
+       if(np == NULL) return;
+       do {
+           hashtab[hashval] = NULL;
+           if(strcmp(np->name, name) != 0){
+               install(np->name, np->defn);
+               //no idea if this is needed or should be here
+               free((void *) np);
+           }
+       }
+       while((np = np->next) != NULL);
+    }
+}
+
+int main(){
+    char line[MAXWORD];
+    int lineCount = 1;
+    int len = 0;
+
+    while((len = getLine(line, MAXWORD)) > 0){
+        if(strncmp("#define", line, 7)){
+
+        }
+    }
+}
