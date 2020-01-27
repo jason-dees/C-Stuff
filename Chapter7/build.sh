fbcb351f3e235eecb5f501d3de9818bbdaba8e55
Jason Dees <jhdees@gmail.com>
2020-01-26 20:00:18 -0500

    start chapter 7

---
+++ Chapter7/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+chapter=7
+SHARED=""
+echo "including: ${SHARED}"
+case "${1}-${2}" in
+    "7-1")
+        echo "building ./Chapter${1}/${1}-${2}.c to ./bin/${1}-${2}-upper.out"
+        cc ./Chapter${1}/${1}-${2}.c ${SHARED} -o ./bin/${1}-${2}-upper.out
+        echo "building ./Chapter${1}/${1}-${2}.c to ./bin/${1}-${2}-lower.out"
+        cc ./Chapter${1}/${1}-${2}.c ${SHARED} -o ./bin/${1}-${2}-lower.out
+        break;
+esac
+echo "building ./Chapter${1}/${1}-${2}.c to ./bin/${1}-${2}.out"
+cc ./Chapter${1}/${1}-${2}.c ${SHARED} -o ./bin/${1}-${2}.out
