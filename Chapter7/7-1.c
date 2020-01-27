04e5de57492f740e17f2cd1e0ad5fa3aeeb4a041
Jason Dees <jhdees@gmail.com>
2020-01-26 20:20:56 -0500

    Update 7-1.c

--- Chapter7/7-1.c
+++ Chapter7/7-1.c
@@ -1,7 +1,15 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 /*
 Write a program that converts upper case to lower or lower case to upper, depending on the name it is invoked with, as 
 found in argv[0].
 */
 
 int main(int argc, char *argv[]){
+    int c;
+    int (*action)(int c) = (strchr(argv[0], 'u') != NULL ? toupper : tolower);
+    while((c = getchar()) != EOF)
+        putchar(action(c));
+    return 0;
 }
