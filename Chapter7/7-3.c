04c7de532a170a1d1912898424f5c1872598de0c
Jason Dees <jdees@pureromance.com>
2020-01-27 11:23:57 -0500

    Update 7-3.c

--- Chapter7/7-3.c
+++ Chapter7/7-3.c
@@ -8,13 +8,32 @@ void minprintf(char *fmt, ...){
     double dval;
     va_start(ap, fmt);
     for(p = fmt; *p; p++){
-        // check *p and *(p+1) for 2 character stuff
         if(*p != '%') {
             putchar(*p);
             continue;
         }
         switch(*++p) {
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+            case '0':
+                break;
+            case '.'://total length
+                break;
+            case '-'://pad end with spaces
+                break;
+            case 'c':
+                ival = va_arg(ap, int);
+                printf("%c", ival);
+                break;
             case 'd':
+            case 'i':
                 ival = va_arg(ap, int);
                 printf("%d", ival);
                 break;
@@ -40,5 +59,15 @@ Revise minprintf to handle more of the other facilities of printf.
 */
 
 int main(int argc, char *argv[]) {
-    printf("%.1s", "hello, world");
+    char *indexStr = "1234567890";
+    printf("1 %-5.10s:\n", indexStr);
+    printf("2 %-15s:\n", indexStr);
+    printf("3 %-15d:\n", 5);
+    printf("4 %-10.5d:\n", 5);
+    printf("5 %10.5d:\n", 5);
+    printf("6 %-10.10s:\n", indexStr);
+    printf("7 %10.10s:\n", indexStr);
+    printf("8 %.20s:\n", indexStr);
+    printf("9 %-20s:\n", indexStr);
+    printf("0 %20s:\n", indexStr);
 }
